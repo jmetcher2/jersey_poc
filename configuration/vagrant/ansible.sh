8e61dbfd55bcfec1225d3efc70d1472d525fff99
Jaime Metcher <jmetcher@gmail.com>
2018-02-14 18:05:09 +1100

    Generate ansible inventory file from vagrant configuration

--- configuration/vagrant/ansible.sh
+++ configuration/vagrant/ansible.sh
@@ -4,3 +4,6 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
+
+# Find the IP address of the virtualbox host
+sudo netstat -r | grep 'default' | awk '{print $2, "vbhost"}' >> /etc/hosts
