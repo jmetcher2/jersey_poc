5c86bc8c14f3d7837703f9e8f86ed025bb43066b
Jaime Metcher <jmetcher@gmail.com>
2018-04-16 22:09:19 +1000

    Update configuration layout

--- configuration/vagrant/provision/ansible.sh
+++ configuration/vagrant/provision/ansible.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y python-software-properties
+sudo apt-get install -y python-software-properties python-dnspython python-boto3 python-botocore
 sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
@@ -9,9 +9,9 @@ sudo apt-get install -y ansible
 sudo netstat -r | grep 'default' | awk '{print $2, "vbhost"}' >> /etc/hosts
 
 # handy script to run the main ansible playbook
-cp /vagrant/aprun.sh .
+cp /vagrant/provision/aprun.sh .
 chmod 775 aprun.sh
 
 # script to update known_hosts from the ansible inventory
-cp /vagrant/ansible-known_hosts.sh .
+cp /vagrant/provision/ansible-known_hosts.sh .
 chmod 775 ansible-known_hosts.sh
